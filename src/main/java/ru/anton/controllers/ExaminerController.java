916135c181df99b36b6e893376e8d80538ee3947
Anton Zhiliakov <gofranich@gmail.com>
2021-02-04 16:01:32 +0600

    Add style.css, make next question, add main(home) page

--- src/main/java/ru/anton/controllers/ExaminerController.java
+++ src/main/java/ru/anton/controllers/ExaminerController.java
@@ -5,24 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.anton.services.QuestionDataService;
 
 @Controller
+@RequestMapping("/tests")
 public class ExaminerController {
 
     @Autowired
     QuestionDataService questionDataService;
 
-    @GetMapping("/")
-    public String home(Model model) {
+    @GetMapping()
+    public String home(){
+        return "/tests/home";
+    }
+
+    @GetMapping("/all")
+    public String questions(Model model) {
         model.addAttribute("allQuestions", questionDataService.getAllQuestion());
-        return "home";
+        return "tests/questions";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model){
         model.addAttribute("question", questionDataService.showSingleQuestion(id));
-        return "/show";
+        return "tests/index";
     }
 
 }
